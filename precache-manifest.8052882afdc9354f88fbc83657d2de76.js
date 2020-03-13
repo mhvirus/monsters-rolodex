665a8fe3ad16cb7d7429aca5ca106ac245f814c3
mhvirus <harisqasim78@gmail.com>
2020-03-14 01:33:48 +0500

    Updates

---
+++ precache-manifest.8052882afdc9354f88fbc83657d2de76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d619c055e126ecfbd9d08c350ffd0e2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c8df31f7cc3d8ff556d9",
+    "url": "/monsters-rolodex/static/css/main.49ca6ec2.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8df31f7cc3d8ff556d9",
+    "url": "/monsters-rolodex/static/js/main.838d2cbc.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
